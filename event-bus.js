7bee0698b214b0acba51f5f815cd6d7f1ee0c3ea
Joe Longstreet <joelongstreet@gmail.com>
2016-02-22 12:31:56 -0600

    write a placeholder event bus

---
+++ event-bus.js
@@ -0,0 +1,95 @@
+var events      = require('events');
+var emitter     = new events.EventEmitter();
+var camera    = require('./lib/camera');
+var webSocket = null;
+var tcpSocket = null;
+
+
+
+// # SOCKET SETUP
+exports.setWebSocket = function(socket){ webSocket = socket; };
+exports.setTcpSocket = function(socket){ tcpSocket = socket; };
+
+
+
+
+// # CAMERA EVENTS
+exports.cameraConnected = function(){
+  console.log('camera connected');
+  webSocket.sockets.emit('camera', {
+    status : 'camera:ready', humanTitle : 'Camera Ready'
+  });
+  camera.startLiveView();
+};
+
+
+exports.cameraRecording = function(){
+  webSocket.sockets.emit('camera', {
+    status : 'camera:recording', humanTitle : 'Camera Recording'
+  });
+};
+
+
+exports.cameraDoneRecording = function(){
+  // create an output directory with random id
+  var id = new Date().getTime().toString();
+  var outputDir = path.join(process.cwd(), 'tmp', id);
+  fs.mkdirSync(outputDir);
+
+  // timeout is necessary to give the camera enough time to "stop recording"
+  setTimeout(function(){
+
+    // tell the client we're done recording
+    io.sockets.emit('camera', {
+      status : 'camera:done-recording', humanTitle : 'Camera Done Recording'
+    });
+
+    camera.writeLastVideoToDisk(outputDir, function(filePath){
+      // let everyone know we're done downloading the file
+      emitter.emit('video-downloaded', filePath);
+
+      // restart the live view and tell the client
+      camera.startLiveView();
+      io.sockets.emit('camera', {
+        status : 'camera:ready', humanTitle : 'Camera Ready'
+      });
+
+    });
+  }, 2500);
+};
+
+
+
+
+// # EVENT BUS MESSAGES
+exports.launchEditor = function(){
+  // TODO: how do i launch an OF binary with flags?
+};
+
+
+
+
+// # WEB PAGE INTERFACE
+exports.initializeWebpageEvents = function(socket){
+
+  // listen for countdown completion from the client before recording
+  socket.on('countdown-done', function(){
+    setTimeout(function(){
+      camera.takeVideo();
+    }, 500);
+  });
+};
+
+
+
+
+// # TURNTABLE INTERFACE
+exports.turntableUpdate = function(input, value, guid){
+  var message = `${guid}|${input.name}|${value}`;
+  if(tcpSocket) tcpSocket.write(message);
+};
+
+
+
+
+exports.events = emitter;
--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-var net = require('net');
+var express   = require('express');
+var app       = express();
+var server    = require('http').createServer(app);
+var io        = require('socket.io')(server);
+var camera    = require('./lib/camera');
+var bus       = require('./event-bus');
 var turntable = require('./lib/turntable')();
-var socket;
 
 
-// setup a tcp server
-var server = net.createServer(function(sock) {
-  console.log('tcp client connected');
-  socket = sock;
+// setup a tcp server for the editing interface
+var tcpServer = require('net').createServer(function(socket){
+  bus.setTcpSocket(socket);
   socket.pipe(socket);
 });
+tcpServer.listen(process.env.TCP_PORT || 3001, '127.0.0.1');
 
-server.listen(3000, '127.0.0.1');
+// configure express
+app.use(express.static('public'));
+app.use(express.static('tmp'));
+server.listen(process.env.PORT || '3000');
 
+// setup the event bus and listen for it's events
+bus.setWebSocket(io);
+bus.events.on('video-downloaded', bus.launchEditor);
 
+// connect camera and listen for events
+camera.setSocket(io);
+camera.connect();
+camera.events.on('connected', bus.cameraConnected);
+camera.events.on('recording', bus.cameraRecording);
+camera.events.on('done-recording', bus.cameraDoneRecording);
 
-// listen to updates from the turntable, then send over via socket
-turntable.on('input-update', function(input, value, guid){
-  var message = `${guid}|${input.name}|${value}`;
-  if(socket) socket.write(message);
-});
+// events from the web page interface
+io.on('connection', bus.initializeWebpageEvents);
+
+// listen to updates from the turntable, then send over via tcp socket
+turntable.on('input-update', bus.turntableUpdate);

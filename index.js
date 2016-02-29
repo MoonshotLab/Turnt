66b63a600a307786fc54025b9a7d006b3ef7696e
Joe Longstreet <joelongstreet@gmail.com>
2016-02-29 11:28:52 -0600

    midi library hates jade

--- index.js
+++ index.js
@@ -20,15 +20,14 @@ app.use(express.static('public'));
 app.use(express.static('tmp'));
 server.listen(config.PORT);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 
 // join the gopro network
-// utils.joinWifi('gopro').then(function(){
-//   gopro.setup();
-//   display.debug('Ready');
-//   display.showScreen('ready');
-// });
+utils.joinWifi('gopro').then(function(){
+  gopro.setup();
+  display.debug('Ready');
+  display.showScreen('ready');
+});
 
 
 
@@ -39,14 +38,14 @@ var editor    = require('./lib/editor');
 var display   = require('./lib/display');
 var gopro     = require('./lib/gopro');
 var video     = require('./lib/video');
-// var turntable = require('./lib/turntable');
+var turntable = require('./lib/turntable');
 
 // give the interface a web socket to use
 display.setWebSocket(io);
 
 // have the display module listen for events from the webpage
 io.on('connection', display.listen);
-// gopro.startLiveStream();
+gopro.startLiveStream();
 
 // when a physical button press comes from the arduino
 arduino.events.on('start', function(){
@@ -69,9 +68,9 @@ gopro.events.on('done-recording', function(){
 });
 
 // when the turntable detects some new input
-// turntable.events.on('input-update', function(data){
-//   editor.turntableUpdate(data);
-// });
+turntable.events.on('input-update', function(data){
+  editor.turntableUpdate(data);
+});
 
 // when the external editor is all done
 editor.events.on('editing-complete', function(){

2b41d2280d60f9b625533efd0eb13b7aa3620575
Joe Longstreet <joelongstreet@gmail.com>
2016-03-04 10:17:49 -0600

    write the showcase interface

--- public/showcase.js
+++ public/showcase.js
@@ -1,27 +1,76 @@
+var refetchIntervals = [null, null];
+var displayOptions   = {};
+
 $(function(){
   // different mointors have different screen arangements
   // for the showcase videos
   var query   = window.location.search;
   var params  = query.slice(1, query.length).split('&');
 
-  $('body').addClass('screen-' + params.screen);
+  params.forEach(function(param){
+    var splits = param.split('=');
+    displayOptions[splits[0]] = splits[1];
+  });
 
-  playRandomVideo();
-  setTimeout(playRandomVideo, 15000);
+  $('body').addClass('screen-' + displayOptions.screen);
+});
+
+
+// listen for incoming videos
+socket.on('new-video', function(data){
+  if(data.screen == displayOptions.screen){
+    var index = getRandomNumber(0, 2);
+    playRandomVideo(index);
+    resetInterval(index);
+  }
 });
 
 
 
+// populate the videos, but delay so nothing plays in sync
+setTimeout(function(){
+  playRandomVideo(0);
+}, getRandomNumber(0, 3000));
+setTimeout(function(){
+  playRandomVideo(1);
+}, getRandomNumber(1000, 10000));
+
+// setup intervals to refetch videos
+resetInterval(0);
+setTimeout(function(){
+  resetInterval(1);
+}, getRandomNumber(1000, 6000));
+
+
+
+// reset the interval for a showcase video evey 30 seconds
+function resetInterval(index){
+  if(refetchIntervals[index]) clearInterval(refetchIntervals[index]);
+
+  refetchIntervals[index] = setInterval(function(){
+    playRandomVideo(index);
+  }, 30000);
+}
+
+
+
 // play a video from the showcase
-var playRandomVideo = function(){
+function playRandomVideo(index){
   $.ajax({
     url : '/showcase?shuffle=true',
     success : function(results){
-      $('.video-one video')[0].src = results[0];
-      $('.video-two video')[0].src = results[1];
-
-      $('.video-one video')[0].play();
-      $('.video-two video')[0].play();
+      $($('.video-player')[index]).addClass('hide');
+      setTimeout(function(){
+        $('video')[index].src = results[0] + '.mp4';
+        $('video')[index].play();
+        $($('.video-player')[index]).removeClass('hide');
+      }, 500);
     }
   });
-};
+}
+
+
+
+function getRandomNumber(lo, hi){
+  return Math.floor(Math.random() * hi) + lo;
+}
